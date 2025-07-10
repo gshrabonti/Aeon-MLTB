fa73be53271911487e1561e987cf053c8dff9dd0
Shrabonti Ghosh <mighosh24@gmail.com>
2025-07-10 22:35:44 +0600

    Update update.py

--- update.py
+++ update.py
@@ -106,13 +106,13 @@ if DATABASE_URL:
 UPSTREAM_REPO = (
     config_file.get("UPSTREAM_REPO", "")
     or os.getenv("UPSTREAM_REPO", "")
-    or "https://github.com/AeonOrg/Aeon-MLTB"
+    or "https://github.com/gshrabonti/Aeon-MLTB"
 )
 
 UPSTREAM_BRANCH = (
     config_file.get("UPSTREAM_BRANCH", "")
     or os.getenv("UPSTREAM_BRANCH", "")
-    or "main"
+    or "beta"
 )
 
 if UPSTREAM_REPO:
